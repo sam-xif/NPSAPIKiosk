5a147e8c35c24b9e94175f19651e27b3097e0522
Sam Xifaras <s.xifaras999@gmail.com>
2019-06-23 13:37:13 -0400

    Created clean back-and-forth between summary and education pages

--- nps-kiosk-app/src/app/park-learn-page/park-learn-page.component.ts
+++ nps-kiosk-app/src/app/park-learn-page/park-learn-page.component.ts
@@ -3,6 +3,9 @@ import {ADataViewComponent} from "../DataViewComponent";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {NPSAPIClientService} from "../services/npsapiclient.service";
 import {ObjectStoreService} from "../services/object-store.service";
+import {INPSObject} from "../../nps/NPSModel";
+import NPSAPIQueryBuilder from "../../nps/NPSAPIQueryBuilder";
+import {NPSDataAccessStrategyBuilder} from "../../nps/NPSDataAccessStrategy";
 
 @Component({
   selector: 'app-park-learn-page',
@@ -10,6 +13,9 @@ import {ObjectStoreService} from "../services/object-store.service";
   styleUrls: ['./park-learn-page.component.css']
 })
 export class ParkLearnPageComponent extends ADataViewComponent {
+
+  private parkCode: string;
+  private park: INPSObject;
 
   constructor(
     protected route: ActivatedRoute,
@@ -20,9 +26,46 @@ export class ParkLearnPageComponent extends ADataViewComponent {
     super(route, router, apiClient, storeService);
   }
 
+  ngOnInit(): void {
+    this.parkCode = this.route.snapshot.paramMap.get('parkCode');
+    super.ngOnInit();
+  }
+
   fetchData(): void {
+    if (this.receivedObject) {
+      this.park = this.receivedObject;
+      this.store(this.receivedObject);
+    } else {
+      // Fetch park
+      let queryBuilder = new NPSAPIQueryBuilder()
+        .addParkCode(this.parkCode)
+        .longText(true)
+        .setLimit(5)
+        .includeField('images')
+        .from('parks');
+
+      let strategy = new NPSDataAccessStrategyBuilder()
+        .use('default')
+        .build();
+
+      let parkSource = this.apiClient.retrieve(queryBuilder.build(), strategy);
+      parkSource.addOnCompletedHandler((snapshot: Array<INPSObject>) => {
+        if (snapshot.length < 1) {
+          this.router.navigateByUrl('/page-not-found');
+          return;
+        }
+
+        this.park = snapshot[0];
+        this.store(this.park);
+      });
+    }
   }
 
   onParamMapChange(newParamMap: ParamMap) {
+    let parkCode = newParamMap.get('parkCode');
+    if (parkCode != this.parkCode) {
+      this.parkCode = parkCode;
+      this.fetchData();
+    }
   }
 }
